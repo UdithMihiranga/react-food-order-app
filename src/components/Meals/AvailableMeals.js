722a4054446f20004b9348671c1d15e7a09532fd
Udith <udithmihiranga@gmail.com>
2022-12-04 11:07:00 +0530

    Implemented the functionality of fetching data from FireBase database

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,31 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
-
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+import { useEffect, useState } from 'react';
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((meal) => (
+
+    const [meals,setMeals] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            
+            const response = await fetch("https://react-http-3ba5b-default-rtdb.firebaseio.com/meals.json")
+            const responseData = await response.json()
+            const mealsData = []
+            for (const key in responseData) {
+                mealsData.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+            })
+            }
+            setMeals(mealsData)          
+        }
+        fetchData()
+    }, [])
+    const mealsList = meals.map((meal) => (
         <MealItem
             id={meal.id}
             key={meal.id}
@@ -39,6 +34,7 @@ const AvailableMeals = () => {
             price={meal.price}
         />
     ));
+    
 
     return (
         <section className={classes.meals}>
